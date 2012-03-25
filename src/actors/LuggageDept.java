5bbcc9d5097245e14d8c606514dd58fc79ec6d59
Bogdan <confucij@gmail.com>
2012-03-25 20:04:50 +0300

    Gui Add

---
+++ src/actors/LuggageDept.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actors;
+
+import simulation.process.Actor;
+import simulation.widgets.Diagram;
+
+/**
+ *Класс представляющий модель багажного отдела
+ * 
+ * @author lordgal
+ */
+public class LuggageDept extends Actor{
+    
+    public Diagram passengerQueDiag;
+    //public 
+    @Override
+    protected void rule() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
