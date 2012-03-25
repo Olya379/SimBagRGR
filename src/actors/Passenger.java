7c33342986480ebe65f2b7ff26f696a215af5113
Bogdan <confucij@gmail.com>
2012-03-25 19:50:32 +0300

    Gui Add

---
+++ src/actors/Passenger.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actors;
+
+import simulation.process.Actor;
+/**
+ *
+ * @author lordgal
+ */
+public class Passenger extends Actor{
+
+    @Override
+    protected void rule() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
